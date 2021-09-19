dfa1bd5692268a9fcd9440737055f9f8d4173bb9
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-19 22:13:14 +0700

    Seperating result state from the print round

--- src/main/java/com/game/of/life/Game.java
+++ src/main/java/com/game/of/life/Game.java
@@ -23,17 +23,28 @@ public class Game {
     }
     public static void printRound(boolean[][] currentGeneration, boolean[][] nextGeneration, int cycles){
         System.out.println("Round 1: ");
-
+        printState(currentGeneration);
         for (int i = 2; i <= cycles; i++) {
             System.out.println();
             System.out.println("Round " + i + ":");
 
+            printState(nextGeneration);
             boolean[][] temp = nextGeneration;
             nextGeneration = currentGeneration;
             currentGeneration = temp;
-
         }
     }
 
+    // print a generation to the console
+    static void printState(boolean[][] w) {
+        for (int i = 0; i < w.length; i++) {
+            for (int j = 0; j < w[i].length; j++)
+                if (w[i][j] == ALIVE)
+                    System.out.print('X');
+                else
+                    System.out.print('.');
+            System.out.println();
+        }
+    }
 
 }
