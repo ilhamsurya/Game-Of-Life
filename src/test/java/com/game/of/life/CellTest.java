69020b8758eaf4f444b0de4c2cbe36f816052b8f
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-19 10:50:19 +0700

    success test condition, cell will live if having 2-3 neighbor

--- src/test/java/com/game/of/life/CellTest.java
+++ src/test/java/com/game/of/life/CellTest.java
@@ -7,15 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CellTest {
     @Test
     // First Rule: cell will die with less than 2 neighbor (Under Population)
-    public void ShouldDieWithOnlyOneNeighbor(){
-        Cell newCell = new Cell(Cell.LifeState.ALIVE);
-        Cell.LifeState current = newCell.GetNextState(1);
-        assertEquals(Cell.LifeState.DEAD, current);
-    }
-    @Test
+
     public void ShouldDieWithZeroNeighbor(){
         Cell newCell = new Cell(Cell.LifeState.ALIVE);
         Cell.LifeState current = newCell.GetNextState(0);
+        assertEquals(Cell.LifeState.DEAD, current);
+    }
+
+    @Test
+    public void ShouldDieWithOnlyOneNeighbor(){
+        Cell newCell = new Cell(Cell.LifeState.ALIVE);
+        Cell.LifeState current = newCell.GetNextState(1);
         assertEquals(Cell.LifeState.DEAD, current);
     }
 
@@ -26,4 +28,13 @@ public class CellTest {
         Cell.LifeState current = newCell.GetNextState(2);
         assertEquals(Cell.LifeState.ALIVE, current);
     }
+
+    @Test
+    public void ShouldLiveOnWithThreeNeighbor(){
+        Cell newCell = new Cell(Cell.LifeState.ALIVE);
+        Cell.LifeState current = newCell.GetNextState(3);
+        assertEquals(Cell.LifeState.ALIVE, current);
+    }
+
+
 }
