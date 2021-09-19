033a7a4c63dcad74588be0960331dde0049d5d41
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-19 22:14:04 +0700

    creating neighbor conditional, print the new generation

--- src/main/java/com/game/of/life/Game.java
+++ src/main/java/com/game/of/life/Game.java
@@ -27,14 +27,41 @@ public class Game {
         for (int i = 2; i <= cycles; i++) {
             System.out.println();
             System.out.println("Round " + i + ":");
-
+            advanceOneGen(currentGeneration, nextGeneration);
             printState(nextGeneration);
+            // swap current and next generations
             boolean[][] temp = nextGeneration;
             nextGeneration = currentGeneration;
             currentGeneration = temp;
         }
     }
-
+    // compute the number of alive neighbors of a cell
+    static int neighbors(int row, int column, boolean[][] w)
+    {
+        int neighborCount = 0;
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++)
+                if (w[row + i][column + j] == ALIVE)
+                    neighborCount = neighborCount + 1;
+        if (w[row][column] == ALIVE)
+            neighborCount--;
+        return neighborCount;
+    }
+    static void advanceOneGen(boolean[][] wOld, boolean[][] wNew)
+    {
+        int neighborCount;
+        for (int i = 1; i < wOld.length - 1; i++) {
+            for (int j = 1; j < wOld[i].length - 1; j++) {
+                neighborCount = neighbors(i, j, wOld);
+                if (neighborCount == 3)
+                    wNew[i][j] = ALIVE;
+                else if (wOld[i][j] == ALIVE && neighborCount == 2)
+                    wNew[i][j] = ALIVE;
+                else
+                    wNew[i][j] = DEAD;
+            }
+        }
+    }
     // print a generation to the console
     static void printState(boolean[][] w) {
         for (int i = 0; i < w.length; i++) {
