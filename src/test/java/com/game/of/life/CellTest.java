b1a0983312b76b23629dcca470373063a64c100c
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-19 10:03:11 +0700

    Failing test, first condition

--- src/test/java/com/game/of/life/CellTest.java
+++ src/test/java/com/game/of/life/CellTest.java
@@ -1,5 +1,12 @@
 package com.game.of.life;
 
+import org.junit.jupiter.api.Test;
+
 public class CellTest {
+    @Test
+    // First Rule: cell will die with less than 2 neighbor (Under Population)
+    public void ShouldDieWithOnlyOneNeighbor(){
+
+    }
 
 }
