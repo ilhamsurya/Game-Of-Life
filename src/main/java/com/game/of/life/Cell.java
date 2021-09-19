696273d0771cfca2eb4344d346496369cbcdc17f
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-19 10:07:55 +0700

    creating new class Cell

--- src/main/java/com/game/of/life/Cell.java
+++ src/main/java/com/game/of/life/Cell.java
@@ -2,4 +2,14 @@ package com.game.of.life;
 
 public class Cell {
 
+    private LifeState state;
+
+    public Cell(LifeState state) {
+        this.state = state;
+
+    }
+
+    public enum LifeState{
+        ALIVE, DEAD
+    }
 }
