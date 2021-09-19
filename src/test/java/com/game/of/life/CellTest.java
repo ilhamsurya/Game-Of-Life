d7708e4ecd6d1ddb8e703eddc662a08e840ad25d
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-19 10:15:22 +0700

    Creating implementation first condition

--- src/test/java/com/game/of/life/CellTest.java
+++ src/test/java/com/game/of/life/CellTest.java
@@ -2,11 +2,15 @@ package com.game.of.life;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CellTest {
     @Test
     // First Rule: cell will die with less than 2 neighbor (Under Population)
     public void ShouldDieWithOnlyOneNeighbor(){
-
+        Cell newCell = new Cell(Cell.LifeState.ALIVE);
+        Cell.LifeState current = newCell.GetNextState(1);
+        assertEquals(Cell.LifeState.DEAD, current);
     }
 
 }
